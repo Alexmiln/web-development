2f97abebd3d02dc2cc4b7690dee497e7c5378a6e
Alex Miln <alexmiln04@gmail.com>
2021-03-05 01:47:31 +0300

    Modified task2.php

--- lw3/task2/task2.php
+++ lw3/task2/task2.php
@@ -8,9 +8,9 @@ if (preg_match('/^[a-zA-Z][\w]*$/', $parameter) and $parameter !== null)
 }
 else
 {
-    preg_match('/^.*[а-яА-Я]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of Cyrillic alphabet. \n") : null;
-    preg_match('/^[0-9].*$/', $parameter) ? ($errors = $errors . "An identifier cannot start with a digit. \n") : null;
-    preg_match('/^.*[\s\p{P}]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of whitespace or punctuation characters. \n") : null;
+    preg_match('/^.*[а-яА-Я]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of Cyrillic alphabet.\n") : null;
+    preg_match('/^[0-9].*$/', $parameter) ? ($errors = $errors . "An identifier cannot start with a digit.\n") : null;
+    preg_match('/^.*[\s\p{P}]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of whitespace or punctuation characters.\n") : null;
     ($errors == null) ? $errors = "Wow, it's scary to imagine what you've entered. But you can't do that anyway)))" : null;
-    echo isset($parameter[null]) ? "No, '$parameter' is not an identifier. \nReason(s): \n$errors" : "Parameter 'identifier' is not found. \n";
+    echo isset($parameter[null]) ? "No, '$parameter' is not an identifier. \nReason(s): \n$errors" : "Parameter 'identifier' is not found.";
 }
