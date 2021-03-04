966ce6a87a8b5de565532b73e231d7451313ef07
Alex Miln <alexmiln04@gmail.com>
2021-03-05 02:28:38 +0300

    Modified task2.php

--- lw3/task2/task2.php
+++ lw3/task2/task2.php
@@ -11,6 +11,6 @@ else
     preg_match('/^.*[а-яА-Я]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of Cyrillic alphabet.\n") : null;
     preg_match('/^[0-9].*$/', $parameter) ? ($errors = $errors . "An identifier cannot start with a digit.\n") : null;
     preg_match('/^.*[\s\p{P}]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of whitespace or punctuation characters.\n") : null;
-    ($errors == null) ? $errors = "Wow, it's scary to imagine what you've entered. But you can't do that anyway)))" : null;
+    ($errors == null) ? $errors = "Wow, you entered a symbol that I didn't anticipate, but trust me, you can't do that either." : null;
     echo isset($parameter[null]) ? "No, '$parameter' is not an identifier. \nReason(s): \n$errors" : "Parameter 'identifier' is not found.";
 }
