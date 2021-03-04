84a41f344a5fc3e6e268f74435cd0c36a22bc14b
Alex Miln <alexmiln04@gmail.com>
2021-03-05 01:45:40 +0300

    Modified task2.php

--- lw3/task2/task2.php
+++ lw3/task2/task2.php
@@ -1,16 +1,16 @@
 <?php
 
 header("Content-Type: text/plain");
-isset($_GET('identifier')) ? $parameter = $_GET('identifier') : print "Parameter 'identifier' is not found.";
-if preg_match('/^[a-zA-Z][\w]*$/', $parameter)
+$parameter = isset($_GET['identifier']) ? $_GET['identifier'] : null;
+if (preg_match('/^[a-zA-Z][\w]*$/', $parameter) and $parameter !== null)
 {
-    echo ('The \'identifier\' parameter is an identifier.')
+    echo "Yes,'$parameter' is an identifier.";
 }
 else
 {
-    preg_match('/^.*[а-яА-Я]+.*$/', $parameter) ? $errors = $errors . "The identifier cannot consist of Cyrillic alphabet. \n";
-    preg_match('/^[0-9].*$/', $parameter) ? $errors = $errors . "An identificator cannot start with a digit. \n";
-    preg_match('/^.*[\s\p{P}]+.*$/', $parameter) ? $errors = $errors . "The identifier cannot consist of whitespace or punctuation characters. \n";
-    ($errors == null) ? $errors = "Wow, it's scary to imagine what you've entered. But you can't do that anyway)))";
-    echo "Sorry, it\'s not an identificator. \n Reason(s): \n $errors";
+    preg_match('/^.*[а-яА-Я]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of Cyrillic alphabet. \n") : null;
+    preg_match('/^[0-9].*$/', $parameter) ? ($errors = $errors . "An identifier cannot start with a digit. \n") : null;
+    preg_match('/^.*[\s\p{P}]+.*$/', $parameter) ? ($errors = $errors . "An identifier cannot consist of whitespace or punctuation characters. \n") : null;
+    ($errors == null) ? $errors = "Wow, it's scary to imagine what you've entered. But you can't do that anyway)))" : null;
+    echo isset($parameter[null]) ? "No, '$parameter' is not an identifier. \nReason(s): \n$errors" : "Parameter 'identifier' is not found. \n";
 }
