aa1902aa8b9325ead6fe0430c1c75cff64f51e68
Alex Miln <alexmiln04@gmail.com>
2021-03-09 14:09:26 +0300

    Minor edits to the 5th task

--- lw3/task5/task5.php
+++ lw3/task5/task5.php
@@ -4,7 +4,7 @@ header("Content-Type: text/plain");
 $email = isset($_GET['email']) ? $_GET['email'] : null;
  if (($email !== null) and ($email !== ''))
 {
-    echo is_file("C:/web/server2go/htdocs/lw3/task4/data/$email.txt") ? file_get_contents("data/$email.txt") : "The requested data was not found.";
+    echo is_file("C:/web/server2go/htdocs/lw3/task4/data/$email.txt") ? file_get_contents("data/$email.txt") : "The requested data was not found.\n";
 }
 else
 {
